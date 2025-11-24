8a19ced3bb8cfed86a34abdecd7557de358a36ff
fahad <fahadkaji.pro@gmail.com>
2025-11-24 21:05:37 +0600

    first commit

---
+++ JS/basic.js
@@ -0,0 +1,24 @@
+// PRIMITIVE DATA TYPE 
+
+const x = 5;
+const y = true;
+const a = "hello";
+
+let z = x;
+z = 10;
+console.log('x =',x);
+console.log('z = ',z);
+
+
+
+// Those data will give us single value
+
+// NON-PRIMITIVE DATA TYPE
+
+const b = {
+    name : 'kazi fahad'
+};
+const num = [1,2,3,4];
+
+
+// These data will give us memory address not value ;
